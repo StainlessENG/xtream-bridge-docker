77d2f0445dc58f7dbc286d607b315f772671909f
StainlessENG <bigmanuk@gmail.com>
2026-02-08 19:42:13 +0000

    Update server.js

--- server.js
+++ server.js
@@ -378,7 +378,7 @@ app.get('/player_api.php', authenticate, (req, res) => {
         is_trial: '0',
         active_cons: '0',
         created_at: '1640995200',
-        max_connections: '1',
+        max_connections: '5',
         allowed_output_formats: ['m3u8', 'ts']
       },
       server_info: {
